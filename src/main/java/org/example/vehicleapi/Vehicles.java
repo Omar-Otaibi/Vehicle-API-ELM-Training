0850e85a189d5315b997ce26de7f51382d2d34ee
Omar-Otaibi <omaralotabe@gmail.com>
2026-01-25 17:47:15 +0300

    add an entity to vehicles class

--- src/main/java/org/example/vehicleapi/Vehicles.java
+++ src/main/java/org/example/vehicleapi/Vehicles.java
@@ -1,5 +1,6 @@
 package org.example.vehicleapi;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Vehicles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( length = 20, nullable = false)
     private String brand;
+    @Column(length = 20)
     private String model;
+    @Column(length = 4)
     private int year;
+    @Column(length = 6, unique = true)
     private String plate;
+    @Column(length = 17, unique = true)
     private String vin;
 }
