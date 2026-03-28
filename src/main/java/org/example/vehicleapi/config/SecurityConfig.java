8f563ee5ac2ed32ba57d42381d8b15a7d3a4d85f
Omar <omaralotabe@gmail.com>
2026-03-28 18:12:09 +0300

    added swagger to the project

--- src/main/java/org/example/vehicleapi/config/SecurityConfig.java
+++ src/main/java/org/example/vehicleapi/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig{
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless REST APIs csrf
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/createOwner", "/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
                 //addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); JWT auth
