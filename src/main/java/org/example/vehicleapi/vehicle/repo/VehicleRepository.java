37ad4bd28135b4113a3de6e5a13c924a7f768393
Omar-Otaibi <omaralotabe@gmail.com>
2026-04-01 11:56:44 +0300

    override findById method with entity graph to fetch owner in one query

--- src/main/java/org/example/vehicleapi/vehicle/repo/VehicleRepository.java
+++ src/main/java/org/example/vehicleapi/vehicle/repo/VehicleRepository.java
@@ -12,6 +12,9 @@ import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long>, JpaSpecificationExecutor<Vehicle> {
+    @EntityGraph(attributePaths = {"owner"})
+    Optional<Vehicle> findById(Long id);
+
     List<Vehicle> findByPlateContaining(String plate);
 
     @EntityGraph(attributePaths = {"owner"})
