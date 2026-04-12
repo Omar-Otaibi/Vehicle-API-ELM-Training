9c6eac73e221401a517b6202ca9ea3440d262405
Omar-Otaibi <omaralotabe@gmail.com>
2026-04-12 16:40:57 +0300

    Set up Async Thread Pool

---
+++ src/main/java/org/example/vehicleapi/config/SchedulerConfig.java
@@ -0,0 +1,29 @@
+package org.example.vehicleapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class SchedulerConfig {
+
+    /**
+     *  Custom Thread Pool to prevent single-thread blocking and memory leaks
+     *  */
+    @Bean(name = "jobExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("ScheduledJob-");
+        executor.initialize();
+        return executor;
+    }
+}
